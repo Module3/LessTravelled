6cb0a0390ff9936aceafd7b2e19bbfca2cccc31f
Jeff Adelman <jeffadelman8@yahoo.com>
2014-05-26 14:19:36 -0700

    backbone started

---
+++ app/js/app.js
@@ -0,0 +1,11 @@
+var backbone = require('backbone');
+var _ = require('underscore');
+var $ = require('jquery');
+var Backbone.$ = $;
+
+var Controls = require('./views/controlsView');
+
+var controls = new Controls();
+
+
+
