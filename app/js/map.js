6cfb8375e33060537fd9f872a27601afadc0519a
dzdonov <dzdonov@gmail.com>
2014-05-30 10:40:19 -0700

    created clearMarkers module

--- app/js/map.js
+++ app/js/map.js
@@ -2,47 +2,41 @@ var ResultsView = require('./views/resultsView');
 var RUN = require('./recurseRunner');
 var PR = require('./placeReq');
 var AC = require('./autocomplete');
+var CM = require('./clearMarkers');
 var _ = require('underscore');
 
 var map = null;
 var markers = [];
 
 module.exports.initialize = function() {
-      var mapOptions = {
-        center: new google.maps.LatLng(47.6797, -122.3331),
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        zoom: 10
-      };
+  var mapOptions = {
+    center: new google.maps.LatLng(47.6797, -122.3331),
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+    zoom: 10
+  };
 
-      map = new google.maps.Map(document.getElementById("map"), mapOptions);
-      directionService = new google.maps.DirectionsService();
-      directionsRenderer = new google.maps.DirectionsRenderer({ map: map });
-      AC.autoComplete(map);
-    };
+  map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  directionService = new google.maps.DirectionsService();
+  directionsRenderer = new google.maps.DirectionsRenderer({ map: map });
+  AC.autoComplete(map);
+};
 
-    var clearMarkers = function() {
-        for (var i = 0; i < markers.length; i++ ) {
-          markers[i].setMap(null);
-        }
-      markers = null;
-      markers = [];
-    };
+module.exports.route = function() {
+  CM.clearMarkers(markers);
+  distance = parseFloat(document.getElementById("distance").value);
 
-    module.exports.route = function() {
-      clearMarkers();
-      distance = parseFloat(document.getElementById("distance").value);
 
-      var placeRequest = PR.placeReq();
-      var searchTerm = document.getElementById('search-term').value;
+  var placeRequest = PR.placeReq();
+  var searchTerm = document.getElementById('search-term').value;
 
-      directionService.route(placeRequest, function(result, status) {
-        var userKeyword;
-        if (status == google.maps.DirectionsStatus.OK) {
-          directionsRenderer.setDirections(result);
-          RUN.recurseRunner(result, map, markers);
-        } else {
-          alert("Directions query failed: " + status);
-        }
-        new ResultsView(result, searchTerm);
-      });
-    };
+  directionService.route(placeRequest, function(result, status) {
+    var userKeyword;
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsRenderer.setDirections(result);
+      RUN.recurseRunner(result, map, markers);
+    } else {
+      alert("Directions query failed: " + status);
+    }
+    new ResultsView(result, searchTerm);
+  });
+};
