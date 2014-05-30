c34bc29ed940c2104a9cb45df3324894f6179d26
Lucas Cotner <lc07@uw.edu>
2014-05-29 19:48:37 -0700

    Even More Modular, IF POSSIBLE

--- app/js/map.js
+++ app/js/map.js
@@ -2,12 +2,11 @@ var ResultsView = require('./views/resultsView');
 var R1 = require('./recurse').recurseSuperFast;
 var R2 = require('./recurse').recurseFast;
 var PC = require('./pathChunker');
-
+var W  = require('./wrapperMod');
 var _ = require('underscore');
 
   var map = null;
   var markers = [];
-
 
     module.exports.initialize = function() {
       var mapOptions = {
@@ -93,17 +92,12 @@ var _ = require('underscore');
 
       var searchTerm = document.getElementById('search-term').value;
 
-      // Make the directions place request
       directionService.route(placeRequest, function(result, status) {
         var userKeyword;
         if (status == google.maps.DirectionsStatus.OK) {
           directionsRenderer.setDirections(result);
 
           var pathChunks = PC.pathChunker(result);
-
-          var url = "http://sampleserver1.arcgisonline.com/ArcGIS/rest/services/Geometry/GeometryServer";
-          var svc = new gmaps.ags.GeometryService(url);
-
 
           midPoint1 = Math.ceil(pathChunks.length/5);
           midPoint2 = Math.ceil(pathChunks.length/5*2);
@@ -114,146 +108,48 @@ var _ = require('underscore');
           var r2 = new R2();
     
           r1.recurseSuperFast(0, midPoint1, function(num) {
-            wrapper(num);
+            W.wrapper(num, pathChunks, map, markers);
           });
 
           r1.recurseSuperFast(midPoint1, midPoint2, function(num) {
-            wrapper(num);
+            W.wrapper(num, pathChunks, map, markers);
           });
 
           r1.recurseSuperFast(midPoint2, midPoint3, function(num) {
-            wrapper(num);
+            W.wrapper(num, pathChunks, map, markers);
           });
 
           r1.recurseSuperFast(midPoint3, midPoint4, function(num) {
             console.log("recurseSuperFast 3 step #: " + num);
-            wrapper(num);
+            W.wrapper(num, pathChunks, map, markers);
           });
 
           r1.recurseSuperFast(midPoint4, pathChunks.length, function(num) {
-            wrapper(num);
+            W.wrapper(num, pathChunks, map, markers);
 
           });
           r2.recurseFast(0, midPoint1, function(num) {
             console.log("recurseFast 1 step #: " + num);
-            wrapper(num);
+            W.wrapper(num, pathChunks, map, markers);
           });
 
           r2.recurseFast(midPoint1, midPoint2, function(num) {
-            wrapper(num);
+            W.wrapper(num, pathChunks, map, markers);
           });
 
           r2.recurseFast(midPoint2, midPoint3, function(num) {
             console.log("recurseFast 3 step #: " + num);
-            wrapper(num);
+            W.wrapper(num, pathChunks, map, markers);
           });
 
           r2.recurseFast(midPoint3, midPoint4, function(num) {
-            wrapper(num);
+            W.wrapper(num, pathChunks, map, markers);
           });
 
           r2.recurseFast(midPoint4, pathChunks.length, function(num) {
-            wrapper(num);
+            W.wrapper(num, pathChunks, map, markers);
           });
 
-            
-          function wrapper(count) {
-            var routeBoxer = new RouteBoxer();
-            
-            if ((typeof pathChunks[count][pathChunks.length - 1]  == 'undefined') && (pathChunks[count].length != 35)) {
-              pathChunks[count].splice(pathChunks[count].length - 1, 1);
-            }
-
-
-            var myLineCurrent = new google.maps.Polyline({
-              map: map,
-              path: pathChunks[count],
-              strokeColor: '#00B506',
-              strokeWeight: 0
-            });
-            var params = {
-              geometries: [myLineCurrent],
-              bufferSpatialReference: gmaps.ags.SpatialReference.WEB_MERCATOR,
-              distances: [(document.getElementById("distance").value)],
-              unit: 9035, //miles
-              unionResults: false
-            };
-              var buffResult;
-                svc.buffer(params, function(results, err) {
-                  if (!err) {
-                      //results.geometries[0][0].setMap(map);
-                      buffResult = results.geometries[0][0];
-
-                      var boxes = routeBoxer.box(pathChunks[count], distance);
-                      
-                      console.log("Number of boxes");
-                      console.log(boxes.length);
-
-                      for (var i = 0; i < boxes.length; i++) { 
-                        var bounds = boxes[i]; 
-                        var infowindow = new google.maps.InfoWindow();
-                        var service = new google.maps.places.PlacesService(map);
-
-                        var userKeyword = document.getElementById('search-term').value;
-
-                        var keywordRequest = {
-                          bounds: bounds,
-                          keyword: [userKeyword]
-                        };
-
-                        service.nearbySearch(keywordRequest, callback);
-                        function callback(results, status) {
-                          if (status == google.maps.places.PlacesServiceStatus.OK) {                   
-                            for (var i = 0; i < results.length; i++) {                    
-                              var myLatlng = new google.maps.LatLng(results[i].geometry.location.k,results[i].geometry.location.A);   
-                              if (google.maps.geometry.poly.containsLocation(myLatlng, buffResult)) {
-
-                                createMarker(results[i]);
-                              } else {
-                                //console.log("Didn't get displayed: " + results[i].name);
-                              }
-                            }
-                          }
-                        }
-                        function createMarker(place) {
-                          var placeLoc = place.geometry.location;
-                          var marker = new google.maps.Marker({
-                            map: map,
-                            position: place.geometry.location
-                          });
-
-                          markers.push(marker);
-
-                          google.maps.event.addListener(marker, 'click', function() {
-                            service.getDetails(place, function(result, status) {
-                              if (status != google.maps.places.PlacesServiceStatus.OK) {
-                                alert(status);
-                                return;
-                              }
-
-                              console.log("RESULT!!!");
-                              console.log(result);
-                              var list = "<% _.each(result, function(name) { %> <li><%= name %></li> <% }); %>";
-                              
-                              console.log("LIST");
-                              //console.log(list);
-                              console.log(_.template(list, {result: [result.name, result.formatted_phone_number]}));
-                              var placeData = _.template(list, {result: [result.name, result.formatted_phone_number]});
-                              infowindow.setContent(placeData);
-                              infowindow.open(map, marker);
-                              //console.log(result);
-                            })
-                          });
-                        }
-
-                      } 
-                  } else {
-                    console.log("Buffer Error!");
-                  }
-              });
-            
-            }
-          
         } else {
           alert("Directions query failed: " + status);
         }
